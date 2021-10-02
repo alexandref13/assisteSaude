45ac5bcdde138dc88dee6191f11ef70175c8f3d3
Alexandre Rocha <alexandrerocha@MacBook-Air-de-Alexandre.local>
2021-10-02 02:56:30 -0300

    ola

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Alexandre Rocha on 02/10/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
